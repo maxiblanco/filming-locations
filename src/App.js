b72e7e077795bd4d0fd573175a9b455e5bf9ceb9
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-26 17:55:43 +0000

    Add google map, main title, marker and infowindow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,105 @@
-import React from 'react';
+import React, { useState, useCallback, useRef } from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
+import { formatRelative } from 'date-fns';
+import mapStyles from './mapStyles';
+// Components
+import CameraIcon from './components/CameraIcon/index';
+
+const libraries = ['places'];
+
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = {
+  lat: -34.61315,
+  lng: -58.37723,
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 const App = () => {
-  return <div className='App'>Hello World!</div>;
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  const handleMapClick = useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return 'Error loading map.';
+
+  if (!isLoaded) return 'Loading map...';
+
+  return (
+    <div>
+      <h1 className='absolute z-10 top-0 left-0 p-0 m-0 mt-4 ml-4 text-2xl'>
+        Film Here <CameraIcon />
+      </h1>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+        options={options}
+        onClick={handleMapClick}
+        onLoad={onMapLoad}>
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: '/clapper.svg',
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}>
+            <div>
+              <h2>Hi Peeps</h2>
+              <p>This is a good place to shoot!</p>
+              <p>Verified last {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </div>
+  );
 };
 
 export default App;
